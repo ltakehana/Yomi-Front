bb6c53a1c1556162ba373d96f110b29d3c5d2bb2
Leonardo Takehana <leost22@gmail.com>
2021-03-28 19:04:30 -0300

    Fix responsivity

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -5,16 +5,12 @@ import google from "../assets/googleIcon.svg";
 import micro from "../assets/microsoftIcon.svg";
 import logo from "../assets/logo_purple.svg";
 
+
 function Cadastro() {
 	return (
 		<div className="container">
-			<div className="left-content">
-				<div className="logo">
-					<img src={logo} alt="Yomi icon" />
-				</div>
-			</div>
-
-			<div className="right-content">
+			<div className="left-content-register">
+				<div id="registerContainer">
 				<div className="title">
 					<p>
 						<strong>Crie sua conta!!!</strong>
@@ -23,13 +19,13 @@ function Cadastro() {
 
 				<form>
 					<label htmlFor="nome">Nome: </label>
-					<input id="nome" type="nome" />
+					<input id="nome" type="input" />
 					<label htmlFor="Email">Email: </label>
 					<input id="email" type="email" />
 					<label htmlFor="Senha">Senha: </label>
-					<input id="senha" type="senha" />
+					<input id="senha" type="password" />
 					<label htmlFor="Email">Confirma senha: </label>
-					<input id="TrueSenha" type="TrueSenha" />
+					<input id="TrueSenha" type="password" />
 					<button type="submit">Cadastre-se</button>
 				</form>
 				<div className="log">
@@ -39,6 +35,12 @@ function Cadastro() {
 					<img src={google} alt="Icon da google" />
 					<img src={face} alt="Icon do face" />
 					<img src={micro} alt="Icon da microsft" />
+				</div>
+				</div>
+			</div>
+			<div className="right-content-register">
+				<div className="logo">
+					<img id="logoRegister" src={logo} alt="Yomi icon" />
 				</div>
 			</div>
 		</div>
