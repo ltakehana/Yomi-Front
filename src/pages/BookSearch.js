edb0c6de2a453f1b430ff40004c65b819b12d7cb
Julia Farias <julia.farias2000@hotmail.com>
2021-03-28 12:21:59 -0300

    commit on '11-BuscaLivro'

--- src/pages/BookSearch.js
+++ src/pages/BookSearch.js
@@ -211,6 +211,22 @@ const BookSearch = (props) => {
       setInvisibleProducts(invisibleElements);
     }
   },[]);
+  const gradeExpandHandle = ()=>{
+    const tmpProductViewCount=(productViewCount+Math.floor(window.innerWidth/288)*3)
+    setProductViewCount(tmpProductViewCount);
+    setProductView(produtos.slice(0,tmpProductViewCount));
+    const tmpProductView = (produtos.slice(0,tmpProductViewCount));
+    if(tmpProductView.length%Math.floor(window.innerWidth/288)==0){
+      setInvisibleProducts([]);
+    }
+    else{
+      let invisibleElements=[];
+      for (let index = 0; index < Math.floor(window.innerWidth/288)-tmpProductView.length%Math.floor(window.innerWidth/288); index++) {
+        invisibleElements.push(<div style={{width:"14em",margin:"2em"}}></div>);
+      }
+      setInvisibleProducts(invisibleElements);
+    }
+  };
 
 
   return (
@@ -220,22 +236,58 @@ const BookSearch = (props) => {
         <Header>
           
         </Header>
-        
-        <div className = "grade-container">
+        <div>
+          <h3 className="grade-title">Resultados</h3>
+        </div>
+        <div>
+        <div className= "searchBox">
+        <ul id="searchContainer" >
+          <p>Editora</p>
+            <li>Galera</li>
+            <li>Intrínseca</li>
+            <li>Rocco</li>
+                 <p>Gênero</p>
+            <li>Terror</li>
+            <li>Romance</li>
+            <li>Comédia</li>
+            <li>Biografia</li>
+                  <p>Autor</p>
+            <li>Ziraldo</li>
+            <li> Rick Riordan</li>
+            <li>Rezende</li>
+            <li>Lucas Neto</li>
+        </ul>
+        </div>
+        <button class ="filter-1">
+            <li>Doações</li>
+        </button>
+        <button class ="filter-2">
+            <li>Vendas</li>
+        </button>
+        <button class ="filter-3">
+            <li>Trocas</li>
+        </button>
+        <div className = "gradeContainer">
           {productView.map((book,index) => (
-            <div className = "bookContainer" key = {index}>
-            <div className = "bookTitle">
+            <div className = "book-container" key = {index}>
+            <div className = "book-title">
             <img src       = {book.img}/>
                 <label>{book.name}</label>
               </div>
+              
               </div>
           ))
-}
+        }
+        {(invisibleProducts)}
+        <button onClick={()=>{gradeExpandHandle()}} className="grade-expand">&middot;&middot;&middot;</button>
+      </div>
+  </div>
+
+
             
    
      </div>
      
-     </div>
   );
 }
 
