a1c2f44151a7bc7eab4c6bd65f05443739fe5782
Yuri Alves <yurivalerio12@gmail.com>
2021-04-29 18:29:03 -0300

    routes fix

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,17 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
-import Cadastro from "../pages/Cadastro";
-import Login from "../pages/Login";
 import BookSearch from "../pages/BookSearch";
-import Chat from "../pages/Chat";
+
 const AppRoutes = () => (
 	<Switch>
 		<Route component={Home} path="/" exact />
-		<Route component={Cadastro} path="/cadastro" exact />
-		<Route component={Login} path="/Login" exact />
 		<Route component={BookSearch} path="/search" exact />
-		<Route component={Chat} path="/chat" exact />
 	</Switch>
 );
 export default AppRoutes;
