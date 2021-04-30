b722f6c3160047fd7c17a1d03946bcc8222717c1
Yuri Alves <yurivalerio12@gmail.com>
2021-04-30 12:08:38 -0300

    routes fix

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
+import PageAnnouncement from "../pages/PageAnnouncement";
 
 const AppRoutes = () => (
 	<Switch>
 		<Route component={Home} path="/" exact />
+		<Route component={PageAnnouncement} path="/book" exact />
 	</Switch>
 );
 export default AppRoutes;
