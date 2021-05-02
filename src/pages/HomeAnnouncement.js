8886da9ede4d9b1d1c6a5274b2de26d75df6a92b
Yuri Alves <yurivalerio12@gmail.com>
2021-05-02 18:06:58 -0300

    Announcement home front page

---
+++ src/pages/HomeAnnouncement.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "../styles/components/homeAnnoucement.css";
+import logo from "../assets/logo_purple.svg";
+
+function HomeAnnouncement() {
+	return (
+		<div id="Home-announce-container">
+			<img src={logo} id="Logo-home"></img>
+
+			<div id="Annouce-options">
+				<div id="Announce-title">
+					<p>Selecione a transação desejada</p>
+				</div>
+
+				<div id="Announce-buttons-container">
+					<button className="Announce-buttons">Trocas</button>
+					<button className="Announce-buttons">Vendas</button>
+					<button className="Announce-buttons">Doações</button>
+				</div>
+
+				<div className="NextButton">
+					<p>Avançar</p>
+					<div className="arrowNext">
+						<span className="material-icons">
+							arrow_forward_ios
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default HomeAnnouncement;
