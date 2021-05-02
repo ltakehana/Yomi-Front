dd7a3b975905cd71851c28a14eb3f60d01d1aa16
Artur_Sousa <artursv2001@gmail.com>
2021-05-02 15:14:52 -0300

    added userNavbar

--- src/pages/cadastroCompra.js
+++ src/pages/cadastroCompra.js
@@ -1,61 +1,68 @@
 import React from "react";
 import "../styles/pages/cadastroCompra.css";
-import HeaderSimple from "../components/HeaderSimple";
+import Header from "../components/Header"
 import Footer from "../components/Footer"
+import UserNavbar from "../components/UserNavbar"
 
 function CadastroCompra(){
     return(
     <html id="html_font">
 
-        <HeaderSimple></HeaderSimple>
+        <Header></Header>
 
-        <body>
-            <div className ="top-elements">
-                        Selecione a transação desejada:
-                        
-                        <button> Trocas </button> <button> Vendas </button> <button> Doações </button>
-                
-            </div>
+        <div className = "publishAnnounceContent">
+            <UserNavbar></UserNavbar>
 
-            <div className = "quadroInformacoes">
+            <div className = "rightElements">
 
-                <div>
-                    <input  className = "titulo" type = "titulo" id="details" placeholder ="Título*"/>
-
-                    <input className = "categoria" type = "Categoria" id = "details" placeholder ="Categoria*"/>
+                <div className ="top-elements">
+                            Selecione a transação desejada:
+                            
+                            <button> Trocas </button> <button> Vendas </button> <button> Doações </button>
+                    
                 </div>
 
-                <div>
-                    <input className = "autor" type = "autor(a)" id = "details" placeholder ="Autor(a)*"/>
-                    <input className = "editora" type = "editora" id = "details" placeholder ="Editora*"/>
-                    <input className = "ano" type = "Ano" id = "details" placeholder ="Ano*"/>
-                    <input className = "paginas" type = "paginas" id = "details" placeholder ="Nº de pág*"/>
-                </div>
+                <div className = "quadroInformacoes">
 
-                <div>
-                    <textarea className = "descricao" type = "descricao" placeholder ="Breve descrição*" ></textarea>
-                </div>            
-                    
-                <div className = "ultimaLinha"> 
+                    <div>
+                        <input  className = "titulo" type = "titulo" id="details" placeholder ="Título*"/>
 
-                    <input className = "cep" type = "cep" id = "details" placeholder ="CEP*"/>
-                    
-
-                    <input className = "preco" type = "preco" id = "details" placeholder ="Preço(R$)*"/>
-                    
-                    <div className = "enviarFoto">
-
-                        <input className = "inputButton" type="file"
-                            id="foto" name="foto"
-                            accept="image/png, image/jpeg"></input>
-                    
+                        <input className = "categoria" type = "Categoria" id = "details" placeholder ="Categoria*"/>
                     </div>
 
-                </div>
+                    <div>
+                        <input className = "autor" type = "autor(a)" id = "details" placeholder ="Autor(a)*"/>
+                        <input className = "editora" type = "editora" id = "details" placeholder ="Editora*"/>
+                        <input className = "ano" type = "Ano" id = "details" placeholder ="Ano*"/>
+                        <input className = "paginas" type = "paginas" id = "details" placeholder ="Nº de pág*"/>
+                    </div>
+
+                    <div>
+                        <textarea className = "descricao" type = "descricao" placeholder ="Breve descrição*" ></textarea>
+                    </div>            
+                        
+                    <div className = "ultimaLinha"> 
+
+                        <input className = "cep" type = "cep" id = "details" placeholder ="CEP*"/>
+                        
+
+                        <input className = "preco" type = "preco" id = "details" placeholder ="Preço(R$)*"/>
+                        
+                        <div className = "enviarFoto">
+
+                            <input className = "inputButton" type="file"
+                                id="foto" name="foto"
+                                accept="image/png, image/jpeg"></input>
+                        
+                        </div>
+
+                    </div>
             
+                </div>
+        
             </div>
         
-        </body>
+        </div>
 
 
         <Footer>
