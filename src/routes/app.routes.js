42f58e10d6efa6b9c7d31f426e155092bc67c84b
Yuri Alves <yurivalerio12@gmail.com>
2021-05-02 12:33:46 -0300

    routes fix

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,26 +1,23 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Router, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
-<<<<<<< HEAD
-import PageAnnouncement from "../pages/PageAnnouncement";
-=======
 import CadastroCompra from "../pages/cadastroCompra";
 import CadastroTroca from "../pages/CadastroTroca";
 import Profile from "../pages/Profile";
->>>>>>> 2b50ef1f5ff3f72c5a97318c6c3b6a0eb4159af4
+import PageAnnouncement from "../pages/PageAnnouncement";
 
 const AppRoutes = () => (
 	<Switch>
 		<Route component={Home} path="/" exact />
-<<<<<<< HEAD
-		<Route component={PageAnnouncement} path="/book" exact />
-	</Switch>
-=======
 		<Route component={Profile} path="/Profile" exact />
-    <Route component={CadastroCompra} path="/Publish_Product" exact/>
-    <Route component={CadastroTroca} path="/Publish_Product_exchange" exact/>
-  </Switch>
->>>>>>> 2b50ef1f5ff3f72c5a97318c6c3b6a0eb4159af4
+		<Route component={CadastroCompra} path="/Publish_Product" exact />
+		<Route
+			component={CadastroTroca}
+			path="/Publish_Product_exchange"
+			exact
+		/>
+		<Route component={PageAnnouncement} path="/Publish_Product" exact />
+	</Switch>
 );
 export default AppRoutes;
