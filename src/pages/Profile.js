e60bccad021dbdd2c53da0d654fc43858c122d69
Leonardo Takehana <leost22@gmail.com>
2021-05-10 02:51:52 -0300

    create base64 profile image input method

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer"
 import UserNavbar from "../components/UserNavbar";
 import "../styles/pages/profile.css"
-import profileImage from "../assets/person-24px 1.png"
+import defaultImage from "../assets/person-24px 1.png"
 import userInfo from "../services/userInfo";
 import updateUser from "../services/updateUser";
 
@@ -17,6 +17,8 @@ const Profile = (props) => {
     const [fieldEmail,setFieldEmail] = useState();
     const [fieldTel,setFieldTel] = useState();
     const [fieldBirth,setFieldBirth] = useState();
+    const [fieldImage,setFieldImage] = useState();
+    const [profileImage,setProfileImage] = useState(defaultImage);
 
     useEffect(async ()=>{
         const responseUserInfo = await userInfo(token);
@@ -40,7 +42,7 @@ const Profile = (props) => {
         let email=null;
         let telephone=null;
         let birth_date=null;
-
+        let profile_image = null;
         if(fieldName)
             name=fieldName;
         if(fieldEmail)
@@ -50,14 +52,35 @@ const Profile = (props) => {
         if(fieldBirth){
             birth_date=fieldBirth;
         }
+        if(fieldImage){
+            profile_image=fieldImage;
+        }
+
         let body={
             "name":name,
             "email":email,
             "username":email,
             "telephone":telephone,
-            "birth_date":birth_date
+            "birth_date":birth_date,
+            "profile_image":profile_image
         };
+        console.log(body);
         updateUser(token,body);
+    };
+
+    const fileToBase64 = async (file) =>
+        new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
+            reader.onload = () => resolve(reader.result)
+            reader.onerror = (e) => reject(e)
+        })
+
+    const handleProfileImage = async (e) =>{
+        let file = e.target.files[0];
+        let imageBase64=await fileToBase64(file)
+        setProfileImage(imageBase64);
+        setFieldImage(imageBase64);
     };
 
 	return (
@@ -72,7 +95,8 @@ const Profile = (props) => {
                     <input id="profileBirthDate" defaultValue={fieldBirth} type="date" title="Data de Nascimento" placeholder="Data de Nascimento" onChange={(e) => setFieldBirth(e.target.value)}/>
                 </div>
                 <div className="profileDiv"> 
-                    <img id="profileImage" src={profileImage}/>
+                    <input id="profileImageInput" onChangeCapture={(e)=>{handleProfileImage(e)}} type="file" accept="image/x-png,image/gif,image/jpeg,image/jpg"/>
+                    <label htmlFor="profileImageInput" style={{cursor:"pointer"}}><img id="profileImage" src={profileImage}/></label>
                 </div>
             </div>
             <div className="profileContent">
