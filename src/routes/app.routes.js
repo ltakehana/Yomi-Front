8d2ee8f7f7ff56734490b54124014c0dd1c050c9
Julia Farias <julia.farias2000@hotmail.com>
2021-05-11 14:04:10 -0300

    commit on 15-Chat

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import CadastroCompra from "../pages/cadastroCompra";
-import CadastroTroca from "../pages/CadastroTroca";
 import Profile from "../pages/Profile";
 import MyAnnouncements from "../pages/MyAnnouncements";
 import Home from "../pages/Home";
@@ -13,8 +11,6 @@ const AppRoutes = () => (
 	<Switch>
 		<Route component={Home} path="/" exact />
 		<Route component={Profile} path="/Profile" exact />
-    <Route component={CadastroCompra} path="/Publish_Product" exact/>
-    <Route component={CadastroTroca} path="/Publish_Product_exchange" exact/>
     <Route component={MyAnnouncements} path="/MyAnnouncements" exact/>
 	<Route component={BookSearch} path="/search" exact />
 		<Route component={Chat} path="/chat" exact />
